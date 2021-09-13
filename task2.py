beb8fe05a7139ad326be09feba2e85ebd964e5cf
evroandrew <volotkov@i.ua>
2021-09-13 23:21:25 +0300

    pep-8

--- task2.py
+++ task2.py
@@ -1,10 +1,10 @@
 import argparse
 
-MSG_next = 'To process the next pair of envelopes print "yes"/"y"'
-MSG_info = 'Parameters of envelopes must be positive numbers. The program determines whether one envelope can be nested inside another.'
-MSG_result_first_in_second = 'The first envelope fits into the second.'
-MSG_result_second_in_first = 'The second envelope fits into the first.'
-MSG_result_none = "No envelope fits inside another."
+msg_next = 'To process the next pair of envelopes print "yes"/"y"'
+msg_info = 'Parameters of envelopes must be positive numbers. The program determines whether one envelope can be nested inside another.'
+msg_result_first_in_second = 'The first envelope fits into the second.'
+msg_result_second_in_first = 'The second envelope fits into the first.'
+msg_result_none = "No envelope fits inside another."
 
 class Envelope:
     def __init__(self, length, width):
@@ -19,9 +19,9 @@ def validation(value):
 
 def env_compare(env1, env2):
     if env1.length > env2.length:
-        message = MSG_result_second_in_first if env1.width > env2.width else RESULT_NONE_MSG
+        message = msg_result_second_in_first if env1.width > env2.width else msg_result_none
     else:        
-        message = MSG_result_first_in_second if env1.width < env2.width else RESULT_NONE_MSG
+        message = msg_result_first_in_second if env1.width < env2.width else msg_result_none
     return message
 
 while True:
@@ -31,11 +31,11 @@ while True:
         length2 = validation(input('Enter c:'))
         width2 = validation(input('Enter d:'))
     except ValueError:
-        print(MSG_info)
+        print(msg_info)
         continue
     env1 = Envelope(length1, width1)
     env2 = Envelope(length2, width2)
-    print(Env_compare(env1, env2))
-    answ_next = input(MSG_next).lower()
+    print(env_compare(env1, env2))
+    answ_next = input(msg_next).lower()
     if answ_next != 'yes' and answ_next != 'y':
         break
