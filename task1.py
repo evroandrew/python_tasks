d1d6a1c4c2fd3866e1728110487e57ea7774bcfd
evroandrew <volotkov@i.ua>
2021-09-13 19:37:34 +0300

    simplified algorithm

--- task1.py
+++ task1.py
@@ -12,13 +12,12 @@ class Chessboard:
         chessboard = ''
         for row in range(self.rows):
             inner = []
-            for col in range(int(self.cols/2)):
-                inner.append('*')
+            el = '*'
             new_line = '\n'
             if row % 2 == 1:
-                chessboard = f"{chessboard}{(''.join([f' {el}' for el in inner]))}{new_line}"
+                chessboard = f"{chessboard}{(''.join([f' {el}' for col in range(int(self.cols/2))]))}{new_line}"
             else:
-                chessboard = f"{chessboard}{(' '.join([f'{el}' for el in inner]))}{new_line}"
+                chessboard = f"{chessboard}{(' '.join([f'{el}' for col in range(int(self.cols/2))]))}{new_line}"
         return chessboard
 
     def show_board(self):
