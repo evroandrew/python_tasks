682de400ff1c75a8c593169e53cd94f784728263
evroandrew <volotkov@i.ua>
2021-09-16 17:29:51 +0300

    add external class validation task1

--- task1.py
+++ task1.py
@@ -1,6 +1,8 @@
 import argparse
+from Validation import Validation
 
 instructions = "In this program we can create a chessboard. For this you should enter two positive integers."
+
 
 class Chessboard:
     def __init__(self, rows=None, cols=None):
@@ -13,22 +15,27 @@ class Chessboard:
             el, cell).create_board(self.rows, self.cols)
         print(chessboard)
 
-class ChessboardGenerator:    
+
+class ChessboardGenerator:
     def __init__(self, el='*', cell=' '):
         self.el = el
-        self.cell = cell        
+        self.cell = cell
         self.new_line = '\n'
-    
-    def create_board(self, rows, cols):        
+
+    def create_lines(self, cols):
+        cols_range = cols // 2
+        if cols % 2 == 1:
+            cols_range += 1
+        even_line = (self.el.join([f'{self.cell}' for col in range(cols_range)]))
+        odd_line = (self.cell.join([f'{self.el}' for col in range(cols_range)]))
+        if cols % 2 == 0:
+            even_line += self.el
+            odd_line += self.cell
+        return odd_line, even_line
+
+    def create_board(self, rows, cols):
+        odd_line, even_line = self.create_lines(cols)
         chessboard = ''
-        cols_range=cols//2
-        if cols%2==1:
-            cols_range+=1
-        even_line=(self.el.join([f'{self.cell}' for col in range(cols_range)]))
-        odd_line=(self.cell.join([f'{self.el}' for col in range(cols_range)]))
-        if cols%2==0:
-            even_line+=self.el
-            odd_line+=self.cell
         for row in range(rows):
             if row % 2 == 1:
                 chessboard = f"{chessboard}{odd_line}{self.new_line}"
@@ -36,19 +43,13 @@ class ChessboardGenerator:
                 chessboard = f"{chessboard}{even_line}{self.new_line}"
         return chessboard
 
-def validation(value):
-    value = int(value)
-    if value < 0:
-        raise ValueError
-    return value
 
 if __name__ == '__main__':
     try:
         parser = argparse.ArgumentParser(description='Enter two arguments - rows and cols for chessboard')
-        parser.add_argument('rows', type=validation, help='Rows number')
-        parser.add_argument('cols', type=validation, help='Cols number')
+        parser.add_argument('rows', type=Validation.positive_integer_validation, help='Rows number')
+        parser.add_argument('cols', type=Validation.positive_integer_validation, help='Cols number')
         args = parser.parse_args()
-        chessboard=Chessboard(args.rows, args.cols)
-        chessboard.show_board('1','2')
+        chessboard = Chessboard(args.rows, args.cols)
     except SystemExit:
         print(instructions)
