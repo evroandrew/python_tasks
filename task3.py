54dcc5743a4ed91fb148fee87bee58c0ac37b738
evroandrew <volotkov@i.ua>
2021-09-16 20:39:39 +0300

    add external class validation task3

--- task3.py
+++ task3.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from Validation import Validation
 
 msg_next = 'To add new triangle print "yes"/"y"'
 msg_info = 'Enter triangle name and three sides separated by commas.'
@@ -12,27 +13,21 @@ class Triangle:
         self.side_b = float(side_b)
         self.side_c = float(side_c)
 
-    def get_square(self):
+    @property
+    def square(self):
         p = (self.side_a + self.side_b + self.side_c) / 2
         area = sqrt(p * (p - self.side_a) *
                     (p - self.side_b) * (p - self.side_c))
         return area
 
 
-def may_exist(sides):
+def triangle_may_exist(sides):
     side_3 = max(sides)
     sides.remove(side_3)
     side_1, side_2 = sides
-    if side_1+side_2 > side_3:
+    if side_1 + side_2 > side_3:
         return True
     return False
-
-
-def validation(args):
-    if len(args) != 4:
-        raise ValueError
-    if any(float(side) < 0 for side in args[1:]):
-        raise ValueError
 
 
 def show_triangles(triangles):
@@ -42,30 +37,32 @@ def show_triangles(triangles):
     else:
         for number, triangle in enumerate(sorted(triangles, key=lambda item: -item[1])):
             print(
-                f'{number+1}. [Triangle {triangle[0]}]: {round(triangle[1], 6)} cm')
+                f'{number + 1}. [Triangle {triangle[0]}]: {round(triangle[1], 6)} cm')
 
 
-triangles = []
-while True:
-    try:
-        triangle_params = input(msg_info).split(',')
-        validation(triangle_params)
-    except ValueError:
-        print(msg_info)
-        continue
-    triangle_params[1], triangle_params[2], triangle_params[3] = float(
-        triangle_params[1]), float(triangle_params[2]), float(triangle_params[3])
-    if may_exist([triangle_params[1], triangle_params[2], triangle_params[3]]):
-        triangle = Triangle(triangle_params[0].strip(
-        ), triangle_params[1], triangle_params[2], triangle_params[3])
-        triangles.append((triangle.name, triangle.get_square()))
+if __name__ == '__main__':
+    triangles = []
+    while True:
+        try:
+            triangle_params = input(msg_info).split(',')
+            Validation.triangle_arguments_validation(triangle_params)
+        except ValueError:
+            print(msg_info)
+            continue
+        triangle_params[1], triangle_params[2], triangle_params[3] = float(
+            triangle_params[1]), float(triangle_params[2]), float(triangle_params[3])
+        if triangle_may_exist([triangle_params[1], triangle_params[2], triangle_params[3]]):
+            triangle = Triangle(triangle_params[0].strip(
+            ), triangle_params[1], triangle_params[2], triangle_params[3])
+            triangles.append((triangle.name, triangle.square))
+        else:
+            print(
+                f'Triangle with such sides ({triangle_params[1]}, {triangle_params[2]}, {triangle_params[3]}) does '
+                f'not exist')
+        answer_next = input(msg_next).lower()
+        if answer_next != 'yes' and answer_next != 'y':
+            break
+    if len(triangles) > 0:
+        show_triangles(triangles)
     else:
-        print(
-            f'Triangle with such sides ({triangle_params[1]}, {triangle_params[2]}, {triangle_params[3]}) does not exist')
-    answ_next = input(msg_next).lower()
-    if answ_next != 'yes' and answ_next != 'y':
-        break
-if len(triangles) > 0:
-    show_triangles(triangles)
-else:
-    show_triangles(msg_result_none)
+        show_triangles(msg_result_none)
