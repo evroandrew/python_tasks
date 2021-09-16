13b29232754fef94335a9d66ae8755389a7a27d0
evroandrew <volotkov@i.ua>
2021-09-16 21:24:03 +0300

    FileWorker

---
+++ FileWorker.py
@@ -0,0 +1,21 @@
+class FileWorker:
+    msg_error = "File does not exist or access is denied. Please, try again."
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_file(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                data = file.read()
+                return data
+        except FileNotFoundError:
+            print(self.msg_error)
+            return []
+
+    def write_file(self, data):
+        try:
+            with open(self.file_path, 'w') as f:
+                f.write(data)
+        except FileNotFoundError:
+            print(self.msg_error)
