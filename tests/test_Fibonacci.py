6899f0ac9aea0b0e397ba7e57d63e329e0e28d71
evroandrew <volotkov@i.ua>
2021-09-28 15:33:05 +0300

    fix naming

--- tests/test_Fibonacci.py
+++ tests/test_Fibonacci.py
@@ -1,25 +1,25 @@
 import unittest
-import Fibonacci
+import _fibonacci
 
 
 class TestFibonacci(unittest.TestCase):
     """
-    This class tests Fibonacci.py
+    This class tests _fibonacci.py
     """
 
     def test_representation(self):
-        self.assertEqual(str(Fibonacci.Fibonacci(10)), '0, 1, 1, 2, 3, 5, 8, 13, 21, 34')
-        self.assertEqual(str(Fibonacci.Fibonacci(20)),
+        self.assertEqual(str(_fibonacci.Fibonacci(10)), '0, 1, 1, 2, 3, 5, 8, 13, 21, 34')
+        self.assertEqual(str(_fibonacci.Fibonacci(20)),
                          '0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181')
-        self.assertEqual(str(Fibonacci.Fibonacci(1)), '0')
-        self.assertEqual(str(Fibonacci.Fibonacci(0)), '')
+        self.assertEqual(str(_fibonacci.Fibonacci(1)), '0')
+        self.assertEqual(str(_fibonacci.Fibonacci(0)), '')
 
     def test_numbers(self):
-        self.assertEqual(list(Fibonacci.Fibonacci(10).numbers()), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
-        self.assertEqual(list(Fibonacci.Fibonacci(20).numbers()),
+        self.assertEqual(list(_fibonacci.Fibonacci(10).numbers()), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+        self.assertEqual(list(_fibonacci.Fibonacci(20).numbers()),
                          [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181])
-        self.assertEqual(list(Fibonacci.Fibonacci(1).numbers()), [0])
-        self.assertEqual(list(Fibonacci.Fibonacci(0).numbers()), [])
+        self.assertEqual(list(_fibonacci.Fibonacci(1).numbers()), [0])
+        self.assertEqual(list(_fibonacci.Fibonacci(0).numbers()), [])
 
 
 if __name__ == '__main__':
--- tests/test_NumbersPairs.py
+++ tests/test_NumbersPairs.py
@@ -1,16 +1,16 @@
 import unittest
-import NumbersPairs
+import _numberpairs
 
 
 class TestNumbersPairs(unittest.TestCase):
     """
-    This class tests NumbersPairs.py
+    This class tests _numberpairs.py
     """
 
     def test_print_pairs(self):
-        print_pairs = NumbersPairs.make_print_pairs_of(10)
+        print_pairs = _numberpairs.make_print_pairs_of(10)
         self.assertEqual(print_pairs(1, 2, 3, 4, 5, 5, 6), print('4+6\n5+5'))
-        print_pairs = NumbersPairs.make_print_pairs_of(7)
+        print_pairs = _numberpairs.make_print_pairs_of(7)
         self.assertEqual(print_pairs(1, 2, 3, 4, 5, 5, 6), print('1+6\n2+5\n2+5\n3+4'))
 
 
